0446af2f6aebe7405469c0049f684fcb1a0341c1
Radu <radu.kovaci@busyweb.nl>
2014-10-23 11:36:27 +0300

    latest changes and new files

---
+++ AddOn Topx Sold Products/lib/config.php
@@ -0,0 +1,28 @@
+<?php
+
+	define ("DB_HOST", "localhost");
+	define ("DB_USER", "root");
+	define ("DB_PASS", "root");
+	define ("DB_NAME", "my_addon_topx");
+	
+	
+	
+	
+	
+	
+	$_array_dummy_data = array(
+		array('260300_03486','20141021','3031','1','loomanrenee@gmail.com','260309'),
+		array('260300_03486','20141021','3032','1','loomanrenee@gmail.com','260309'),
+		array('260300_03486','20141021','3615','1','loomanrenee@gmail.com','260309'),
+		array('260300_03485','20141020','3011','1','s.f.stoffels@online.nl','260303'),
+		array('260300_03484','20141020','3051','1','alzirenebarbosa@hotmail.com','260318'),
+		array('260300_03483','20141022','3024','1','overloon@thomashuis.nl','260315'),
+		array('260300_03483','20141022','3032','1','overloon@thomashuis.nl','260315'),
+		array('260300_03482','20141023','3024','1','overloon@thomashuis.nl','260315'),
+		array('260300_03482','20141023','3032','1','overloon@thomashuis.nl','260315'),
+		array('260300_03481','20141020','3619','1','info@rimpelconsult.nl','260315'),
+		array('260300_03481','20141020','3676','1','info@rimpelconsult.nl','260315'),
+		array('260300_03481','20141020','3606','1','info@rimpelconsult.nl','260315'),
+		array('260300_03481','20141020','3024','1','info@rimpelconsult.nl','260315'),
+		array('260300_03480','20141020','3607','1','ine.berkelmans@gmail.com','260302')
+	);
